989e53469d3b3660f1ad62ce4bbb46b112bc5ed8
william ikeji <williamikeji@gmail.com>
2021-04-03 18:05:10 +0100

    sales pushed to remote

--- backend/src/sales/models.py
+++ backend/src/sales/models.py
@@ -3,12 +3,13 @@ from django.db import models
 # Create your models here.
 class sale(models.Model):
     productId = models.CharField(max_length=20)
+    invoiceId = models.CharField(null=True,max_length=100)
     productName = models.CharField(max_length=50)
     quantity = models.IntegerField()
     price = models.IntegerField()
-    paid = models.IntegerField(default=0)
-    toPay = models.IntegerField(default=0)
-    disccount = models.IntegerField()
+    #paid = models.IntegerField(default=0)
+    #toPay = models.IntegerField(default=0)
+    #disccount = models.IntegerField()
     transactionType = models.CharField(max_length=20)
     companyId = models.CharField(max_length=50,null=True)
     branchId = models.CharField(max_length=50,null=True)
